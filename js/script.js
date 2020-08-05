377bacb4575b1ec18888dd975c7d22112f42d8ca
serii <seriiburduja@gmail.com>
2020-08-05 18:23:46 +0300

    script js

--- js/script.js
+++ js/script.js
@@ -1,24 +1,27 @@
 "use strict";
 
-const numberOfFilms = +prompt('Сколько фильмов вы посмотрели?', '');
-const personalMovieDB = {
-	count: numberOfFilms,
-	movies: {},
-	actors: {},
-	genres: [],
-	private: false
-};
+// const numberOfFilms = +prompt('Сколько фильмов вы посмотрели?', '');
+// const personalMovieDB = {
+// 	count: numberOfFilms,
+// 	movies: {},
+// 	actors: {},
+// 	genres: [],
+// 	private: false
+// };
+//
+// for (let i = 0; i < 2; i++) {
+// 	let a = prompt('Один из поледних просмотренных фильмов?', '');
+// 	let b = prompt('На сколько вы его оцените?', '');
+//
+// 	if (a !== null && b !== null && a !== '' && b !== '' && a.length < 50) {
+// 		personalMovieDB.movies[a] = b;
+// 	}else{
+// 		i--;
+// 	}
+//
+// }
 
-for (let i = 0; i < 2; i++) {
-	let a = prompt('Один из поледних просмотренных фильмов?', '');
-	let b = prompt('На сколько вы его оцените?', '');
+// console.log(personalMovieDB);
 
-	if (a !== null && b !== null && a !== '' && b !== '' && a.length < 50) {
-		personalMovieDB.movies[a] = b;
-	}else{
-		i--;
-	}
-
-}
-
-console.log(personalMovieDB);
+// const calc = a => a * 2;
+// console.log(calc(8));
