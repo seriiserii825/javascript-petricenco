514577bddbb6984b64ce5bec6d03cbcf1f2ae608
serii <seriiburduja@gmail.com>
2020-08-13 10:10:50 +0300

    card

--- Food/js/food.js
+++ Food/js/food.js
@@ -141,6 +141,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 		window.addEventListener('scroll', showModalByScroll);
 	};
+
 	// modal();
 
 	class Card {
@@ -152,57 +153,56 @@ document.addEventListener('DOMContentLoaded', () => {
 			this.priceConvertor = 27;
 			this.alt = alt;
 			this.exchange();
-			this.parrent = parrent;
+			this.parrent = document.querySelector(parrent);
 		}
 
-		exchange(){
+		exchange() {
 			this.price = this.price * this.priceConvertor;
 		}
 
-		render(){
+		render() {
 			const div = document.createElement('div');
 			div.innerHTML = `
-			
+				<div class="menu__item">
+					<img src="${this.imgPath}" alt="vegy">
+					<h3 class="menu__item-subtitle">${this.title}</h3>
+					<div class="menu__item-descr">${this.content}</div>
+					<div class="menu__item-divider"></div>
+					<div class="menu__item-price">
+						<div class="menu__item-cost">Цена:</div>
+						<div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+					</div>
+				</div>
 			`;
+
+			this.parrent.prepend(div);
 		}
 	}
 
-	const cards = [
-		{
-			title: 'Меню "Фитнес"',
-			imgPath: "img/tabs/vegy.jpg",
-			content: 'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
-			price: 229
-		},
-		{
-			title: 'Меню “Премиум”',
-			imgPath: "img/tabs/elite.jpg",
-			content: 'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
-			price: 550
-		},
-		{
-			title: 'Меню "Постное"',
-			imgPath: "img/tabs/post.jpg",
-			content: 'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
-			price: 430
-		},
-	];
+	new Card(
+		'Меню "Фитнес"',
+		"img/tabs/vegy.jpg",
+		'alt',
+		'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+		10,
+		'.menu__field .container'
+	).render();
 
-	const menuContainer = document.querySelector('.menu__field .container');
-	let html = '';
-	for (let card of cards) {
-		const cardObject = new Card(card.title, card.imgPath, card.content, card.price);
+	new Card(
+		'Меню “Премиум”',
+		"img/tabs/elite.jpg",
+		'alt',
+		'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+		10,
+		'.menu__field .container'
+	).render();
 
-		html += '<div class="menu__item">';
-		html += `<img src="${cardObject.imgPath}" alt="vegy">`;
-		html += `<h3 class="menu__item-subtitle">${cardObject.title}</h3>`;
-		html += `<div class="menu__item-descr">${cardObject.content}</div>`;
-		html += `<div class="menu__item-divider"></div>`;
-		html += `<div class="menu__item-price">`;
-		html += `<div class="menu__item-cost">Цена:</div>`;
-		html += `<div class="menu__item-total"><span>${cardObject.price}</span> грн/день</div>`;
-		html += `</div>`;
-		html += '</div>';
-	}
-	menuContainer.innerHTML = html;
+	new Card(
+		'Меню "Постное"',
+		"img/tabs/post.jpg",
+		'alt',
+		'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+		7,
+		'.menu__field .container'
+	).render();
 });
