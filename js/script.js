14d98615dc9a91e9e2aad2c346a786ff75f456d6
serii <seriiburduja@gmail.com>
2020-08-19 17:09:26 +0300

    some

--- js/script.js
+++ js/script.js
@@ -12,7 +12,6 @@ employers.forEach((item, i) => {
 employersNames = employersNames.map((item) => {
 	return item.toLowerCase().trim();
 });
-console.log(employersNames);
 
 let sponsors = {
 	cash: [40000, 5000, 30400, 12000],
@@ -34,15 +33,14 @@ function calcCash(own, ...numbers) {
 const sponsorsCache = sponsors.cash.join(', ');
 let money = calcCash(0, sponsorsCache);
 
-function makeBusiness(owner, director, cash, emp) {
-	director = director || 'Victor';
-	let sumSponsors = sponsors.eu.concat(sponsors.rus, 'unexpected sponsor');
+function makeBusiness(owner, cash, emp, director = 'Victor') {
+	let sumSponsors = [...sponsors.eu, ...sponsors.rus, 'unexpected sponsor'];
 	console.log('We have a business. Owner: ' + owner + ', director: ' + director + '. Our budget: ' + cash + '. And our employers: ' +
 		emp);
 	console.log('And we have a sponsors: ');
-	console.log.apply(null, sumSponsors);
+	console.log(sumSponsors.join(', '));
 	console.log('Note. Be careful with ' + sponsors.eu[0] + ". It's a huge risk.");
 }
 
-makeBusiness.apply(null, ['Sam', null, money, employersNames]);
+makeBusiness('Owner', money, employersNames, 'Sam');
 
