eb5535a1006d084cc484c394127fb6e852bf647e
serii <seriiburduja@gmail.com>
2020-09-17 09:37:54 +0300

    some

--- js/script.js
+++ js/script.js
@@ -1,11 +1,2 @@
-const now = new Date();
-
-for(let i = 0; i < 1000000; i++){
-	let some = i;
-}
-
-const end = new Date();
-
-// console.log(`loop worked ${end - now} milliseconds`);
-
-console.log(Math.floor(4 % 60));
+const number = new Number(22);
+console.log(number);
