38f07165fbe845349ed90ca51e031b0870cca124
serii <seriiburduja@gmail.com>
2020-09-21 09:57:55 +0300

    fetch

--- js/script.js
+++ js/script.js
@@ -21,19 +21,30 @@
 // 	.then(json => showJsonData(json));
 
 
-function showJsonPhotos(data) {
-	let ul = document.createElement('ul');
-	data.some((item, i) => {
-		ul.innerHTML += `
-			<li>
-				<img src="${item.url}" alt="">
-			</li>
-		`;
-		return i === 10;
-	});
-	document.body.append(ul);
-}
+// function showJsonPhotos(data) {
+// 	let ul = document.createElement('ul');
+// 	data.some((item, i) => {
+// 		ul.innerHTML += `
+// 			<li>
+// 				<img src="${item.url}" alt="">
+// 			</li>
+// 		`;
+// 		return i === 10;
+// 	});
+// 	document.body.append(ul);
+// }
+//
+// fetch('https://jsonplaceholder.typicode.com/photos')
+// 	.then(response => response.json())
+// 	.then(json => showJsonPhotos(json));
 
-fetch('https://jsonplaceholder.typicode.com/photos')
-	.then(response => response.json())
-	.then(json => showJsonPhotos(json));
+
+fetch('https://jsonplaceholder.typicode.com/posts',{
+		method: "POST",
+		body: JSON.stringify({name: 'Alex'}),
+		headers: {
+			'Content-type': 'application/json'
+		}
+	})
+	.then((response) => response.json())
+	.then(json => console.log(json));
